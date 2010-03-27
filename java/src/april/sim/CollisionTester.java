5149a4f3b9d09bbecea8b6f7252b21923054da80
Edwin Olson <eblson@umich.edu>
2010-03-27 13:58:18 -0400

    remove

--- java/src/april/sim/CollisionTester.java
+++
@@ -1,9 +0,0 @@
-package april.sim;
-
-public interface CollisionTester
-{
-    /** Does a circle positioned at xy with radius 'radius' collide
-     * with something?
-     **/
-    public boolean isCollision(double x, double y, double radius);
-}
