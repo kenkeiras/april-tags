ff5e3278b5e5e98ce6b3fcd9738f9c9a5dff78f8
Johannes H. Strom <jstrom@gmail.com>
2010-05-01 23:41:58 -0400

    Added unit test which exposes tricky bug

--- java/src/april/util/StringUtil.java
+++ java/src/april/util/StringUtil.java
@@ -37,4 +37,58 @@ public class StringUtil
 
         return resolved;
     }
+
+    public static void main(String args[])
+    {
+        // Test cases: Be sure to export USER_HOME before starting
+        // unit test
+
+        String user = "$USER";
+        String user_home = "$USER_home";
+
+        String true_user = System.getenv(user.split("\\W")[1]);
+        if (true_user  == null)
+            true_user = user;
+
+        String true_user_home = System.getenv(user_home.split("\\W")[1]);
+        if (true_user_home  == null)
+            true_user_home = user_home;
+
+        String examples[] = {user,user+" ", " "+user,
+                             "$ USER", "$ USER/"+user, user+"/"+user,
+                             "b"+user+" ", "b"+user, user+"b", user+"/b",
+                             "b"+user_home, user_home+"b", user_home+"/b",
+                             user_home+"/",user + " "+user_home};
+
+        String answers[] = {true_user,true_user+" ", " "+true_user,
+                             "$ USER", "$ USER/"+true_user, true_user+"/"+true_user,
+                             "b"+true_user+" ", "b"+true_user, user+"b", true_user+"/b",
+                             "b"+true_user_home, user_home+"b", true_user_home+"/b",
+                            true_user_home+"/",true_user+" "+true_user_home};
+
+
+        boolean allGood = true;
+        for (int i = 0; i < examples.length; i++) {
+            String src = examples[i];
+            String ans = answers[i];
+            String result = StringUtil.replaceEnvironmentVariables(examples[i]);
+
+            System.out.print("Example "+i);
+            if (ans.equals(result))
+                System.out.print(" SUCCESS ");
+            else{
+                System.out.print(" FAILURE ");
+                allGood = false;
+            }
+            System.out.print(" -- Source: >"+src+"< ");
+            System.out.print("Result: >"+result+"< ");
+            System.out.println("Answer: >"+ans+"<");
+        }
+
+        if (allGood)
+            System.out.println("ALL SYSTEMS GO!");
+        else
+            System.out.println("HOUSTON, WE'VE GOT A PROBLEM!");
+
+    }
 }
