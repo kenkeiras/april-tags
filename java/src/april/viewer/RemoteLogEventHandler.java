72c203cfa37ec952b935d018c5ad37b5a32e2a9a
Edwin Olson <eblson@umich.edu>
2010-05-04 08:39:26 -0800

    add viewer

---
+++ java/src/april/viewer/RemoteLogEventHandler.java
@@ -0,0 +1,75 @@
+package april.viewer;
+
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+
+import april.config.*;
+import april.vis.*;
+
+/** Allows VisCanvas to send events directly to a lcm-logplayer-gui **/
+public class RemoteLogEventHandler extends VisCanvasEventAdapter implements ViewObject
+{
+    Viewer viewer;
+    String name;
+    Config config;
+
+    public RemoteLogEventHandler(Viewer viewer, String name, Config config)
+    {
+        this.viewer = viewer;
+        this.name = name;
+        this.config = config;
+        viewer.getVisCanvas().addEventHandler(this, 0);
+    }
+
+    public String getName()
+    {
+        return "RemoteLogEventHandler";
+    }
+
+    static void udpSendString(String addr, int port, String s)
+    {
+        try
+        {
+            DatagramSocket sock = new DatagramSocket();
+            byte b[] = s.getBytes();
+            DatagramPacket p = new DatagramPacket(b, b.length, InetAddress.getByName(addr), port);
+            sock.send(p);
+        } catch (IOException ex)
+        {
+            System.out.println("Ex: " + ex);
+        }
+    }
+
+    public boolean keyTyped(VisCanvas vc, KeyEvent e)
+    {
+        switch (e.getKeyChar())
+        {
+            case 'P':
+            case 'p':
+                udpSendString("127.0.0.1", 53261, "PLAYPAUSETOGGLE");
+                break;
+            case 'N':
+            case 'n':
+                udpSendString("127.0.0.1", 53261, "STEP");
+                break;
+            case '=':
+            case '+':
+                udpSendString("127.0.0.1", 53261, "FASTER");
+                break;
+            case '_':
+            case '-':
+                udpSendString("127.0.0.1", 53261, "SLOWER");
+                break;
+            case '[':
+                udpSendString("127.0.0.1", 53261, "BACK5");
+                break;
+            case ']':
+                udpSendString("127.0.0.1", 53261, "FORWARD5");
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+}
