c97413747f52d066b3cbd982429ecda2120ac8e6
Edwin Olson <eblson@umich.edu>
2010-05-18 19:55:54 -0400

    plane fitting

--- java/src/april/jmat/LinAlg.java
+++ java/src/april/jmat/LinAlg.java
@@ -795,6 +795,15 @@ public final class LinAlg
         // SLERP.
         return LinAlg.normalize(LinAlg.add(LinAlg.scale(q0, Math.sin(angle*(1-w))),
                                            LinAlg.scale(q1, Math.sin(angle*w))));
+
+        /* Test code template
+           Try -45,-45 ; -45 +45 ; -45 134; -45 136 ; 0 90
+
+        double q0[] = LinAlg.rollPitchYawToQuat(new double[] {0, 0, Math.toRadians(-45)});
+        double q1[] = LinAlg.rollPitchYawToQuat(new double[] {0, 0, Math.toRadians(-45)});
+        for (double w = 0; w <= 1; w+= 0.1)
+            System.out.printf("%15f\n", Math.toDegrees(LinAlg.quatToRollPitchYaw(LinAlg.slerp(q0, q1, w))[2]));
+        */
     }
 
     /** Rotate the vector v by the quaternion q **/
@@ -1989,4 +1998,40 @@ public final class LinAlg
         return MM.det();
     }
 
+    /** Find the best plane fit to a set of points using SVD. **/
+    public static double[] fitPlaneNormal(ArrayList<double[]> points)
+    {
+        double A[][] = new double[3][3];
+        double M[] = new double[3];
+
+        for (double p[] : points) {
+            LinAlg.plusEquals(M, p);
+        }
+
+        for (int i = 0; i < M.length; i++)
+            M[i] /= points.size();
+
+        for (double p[] : points) {
+            double p0 = p[0] - M[0];
+            double p1 = p[1] - M[1];
+            double p2 = p[2] - M[2];
+
+            A[0][0] += p0*p0;
+            A[0][1] += p0*p1;
+            A[0][2] += p0*p2;
+            A[1][1] += p1*p1;
+            A[1][2] += p1*p2;
+            A[2][2] += p2*p2;
+        }
+
+        // make symmetric
+        A[1][0] = A[0][1];
+        A[2][0] = A[0][2];
+        A[2][1] = A[1][2];
+
+        Matrix V = new SingularValueDecomposition(new Matrix(A)).getV();
+        return new double[] { V.get(0, 2),
+                              V.get(1, 2),
+                              V.get(2, 2) };
+    }
 }
