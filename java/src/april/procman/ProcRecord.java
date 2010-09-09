98aac44fecb7f3fcbfc2526880c7b390f294bd1f
Edwin Olson <eblson@umich.edu>
2010-09-09 17:01:01 -0400

    add procman

---
+++ java/src/april/procman/ProcRecord.java
@@ -0,0 +1,15 @@
+package april.procman;
+
+class ProcRecord
+{
+    int    procid;
+    String host;
+    String cmdline;
+
+    int restartCount;
+    int lastExitCode;
+
+    boolean autoRestart; // auto restart this process on exit
+    int     restartDelayMS; // num. millisecs until restarted if failed
+    boolean running; // try to stop the proc.
+}
