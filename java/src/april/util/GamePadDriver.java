f04d9278055513c87c0bcef92144ed07ad588a7e
Ryan Morton <rmorton@umich.edu>
2011-10-24 00:49:50 -0400

    removed unused variables from GamePadDriver

--- java/src/april/util/GamePadDriver.java
+++ java/src/april/util/GamePadDriver.java
@@ -2,7 +2,7 @@ package april.util;
 
 import april.util.*;
 import lcm.lcm.*;
-import april.lcmtypes.*;
+import april.lcmtypes.gamepad_t;
 
 public class GamePadDriver
 {
@@ -20,9 +20,6 @@ public class GamePadDriver
         GamePad gp = new GamePad(true);
         LCM lcm = LCM.getSingleton();
 
-        boolean gotPress = false;
-        long oldTime = 0;
-
         while (true) {
 
             TimeUtil.sleep(gp.isPresent() ? 25 : 250);
@@ -39,9 +36,6 @@ public class GamePadDriver
             for (int i = 0; i < 32; i++)
                 if (gp.getButton(i))
                     msg.buttons |= (1<<i);
-
-            if (msg.buttons > 0)
-                gotPress = true;
 
             msg.present = gp.isPresent();
 
