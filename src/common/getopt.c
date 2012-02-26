e8d123b74311133b74f6188684a30aaa7bcd4fe2
Ryan Morton <rmorton@umich.edu>
2012-02-26 17:59:06 -0500

    fixed memory leak in getopt.c (now properly frees individual elements)

--- src/common/getopt.c
+++ src/common/getopt.c
@@ -9,6 +9,33 @@
 #define GOO_BOOL_TYPE 1
 #define GOO_STRING_TYPE 2
 
+static void getopt_free_hashtable_entry(gpointer key, gpointer value, gpointer user_data)
+{
+    g_free(key);
+    if (user_data) {
+        getopt_option_t *goo = (getopt_option_t *)value;
+
+        free(goo->svalue);
+        free(goo->help);
+        g_free(value);
+    }
+}
+
+static void getopt_free_array_extra(gpointer data, gpointer user_data)
+{
+    if (data != NULL)
+        free((char *)data);
+}
+
+static void getopt_free_array_options(gpointer data, gpointer user_data)
+{
+    getopt_option_t *goo = (getopt_option_t *)data;
+    if (goo != NULL && goo->spacer) {
+        free(goo->help);
+        free(goo);
+    }
+}
+
 getopt_t *getopt_create()
 {
     getopt_t *gopt = (getopt_t*) calloc(1, sizeof(getopt_t));
@@ -22,13 +49,29 @@ getopt_t *getopt_create()
 
 void getopt_destroy(getopt_t *gopt)
 {
-    // XXX We need to free the members
+    // free individual elements first
+    g_ptr_array_foreach(gopt->extraargs, getopt_free_array_extra, (gpointer)0);
+    g_ptr_array_foreach(gopt->options, getopt_free_array_options, (gpointer)0);
+    g_hash_table_foreach(gopt->lopts, getopt_free_hashtable_entry, (gpointer)0);
+    g_hash_table_foreach(gopt->sopts, getopt_free_hashtable_entry, (gpointer)1);
+
+    // free tables
     g_hash_table_destroy(gopt->lopts);
     g_hash_table_destroy(gopt->sopts);
+
+    // free arrays
     g_ptr_array_free(gopt->options, TRUE);
     g_ptr_array_free(gopt->extraargs, TRUE);
 
     free(gopt);
+}
+
+void getopt_modify_string(char **str, char *newvalue)
+{
+    char *old = *str;
+    *str = newvalue;
+    if (old != NULL)
+        free(old);
 }
 
 // returns 1 if no error
@@ -44,7 +87,7 @@ int getopt_parse(getopt_t *gopt, int argc, char *argv[], int showErrors)
 
         // no equal sign? Push the whole thing.
         if (eq == NULL) {
-            g_ptr_array_add(toks, strdup(arg));
+            g_ptr_array_add(toks, arg);
         } else {
             // there was an equal sign. Push the part
             // before and after the equal sign
@@ -59,6 +102,8 @@ int getopt_parse(getopt_t *gopt, int argc, char *argv[], int showErrors)
                 if (val[last]=='\"')
                     val[last] = 0;
                 g_ptr_array_add(toks, &val[1]);
+                val[0] = 0;
+                free(val); // only 1st bit
             } else {
                 g_ptr_array_add(toks, val);
             }
@@ -67,9 +112,16 @@ int getopt_parse(getopt_t *gopt, int argc, char *argv[], int showErrors)
 
     // now loop over the elements and evaluate the arguments
     unsigned int i = 0;
+
+    char *tok = NULL;
+
     while (i < toks->len) {
 
-        char *tok = (char*) g_ptr_array_index(toks, i);
+        // rather than free statement throughout this while loop
+        if (tok != NULL)
+            free(tok);
+
+        tok = (char*) g_ptr_array_index(toks, i);
 
         if (!strncmp(tok,"--", 2)) {
             char *optname = &tok[2];
@@ -88,17 +140,16 @@ int getopt_parse(getopt_t *gopt, int argc, char *argv[], int showErrors)
 
                     if (!strcmp(val,"true")) {
                         i+=2;
-                        goo->svalue = "true";
+                        getopt_modify_string(&goo->svalue, val);
                         continue;
                     }
                     if (!strcmp(val,"false")) {
                         i+=2;
-                        goo->svalue = "false";
+                        getopt_modify_string(&goo->svalue, val);
                         continue;
                     }
                 }
-
-                goo->svalue = "true";
+                getopt_modify_string(&goo->svalue, strdup("true"));
                 i++;
                 continue;
             }
@@ -107,8 +158,7 @@ int getopt_parse(getopt_t *gopt, int argc, char *argv[], int showErrors)
                 if ((i+1) < toks->len) {
                     char *val = (char*) g_ptr_array_index(toks, i+1);
                     i+=2;
-
-                    goo->svalue = strdup(val);
+                    getopt_modify_string(&goo->svalue, val);
                     continue;
                 }
 
@@ -131,6 +181,7 @@ int getopt_parse(getopt_t *gopt, int argc, char *argv[], int showErrors)
                     // is the argument a numerical literal that happens to be negative?
                     if (pos==1 && isdigit(tok[pos])) {
                         g_ptr_array_add(gopt->extraargs, tok);
+                        tok = NULL;
                         break;
                     } else {
                         okay = 0;
@@ -142,7 +193,7 @@ int getopt_parse(getopt_t *gopt, int argc, char *argv[], int showErrors)
                 }
 
                 if (goo->type == GOO_BOOL_TYPE) {
-                    goo->svalue = "true";
+                    getopt_modify_string(&goo->svalue, strdup("true"));
                     continue;
                 }
 
@@ -156,8 +207,7 @@ int getopt_parse(getopt_t *gopt, int argc, char *argv[], int showErrors)
                                 printf("Ran out of arguments for option block %s\n", tok);
                         }
                         i++;
-
-                        goo->svalue=strdup(val);
+                        getopt_modify_string(&goo->svalue, val);
                         continue;
                     }
 
@@ -172,8 +222,11 @@ int getopt_parse(getopt_t *gopt, int argc, char *argv[], int showErrors)
 
         // it's not an option-- it's an argument.
         g_ptr_array_add(gopt->extraargs, tok);
+        tok = NULL;
         i++;
     }
+    if (tok != NULL)
+        free(tok);
 
     return okay;
 }
@@ -232,22 +285,25 @@ char *getopt_get_string(getopt_t *gopt, const char *lname)
     getopt_option_t *goo = (getopt_option_t*) g_hash_table_lookup(gopt->lopts, lname);
     if (goo == NULL)
         return NULL;
-
-    return goo->svalue;
+    return strdup(goo->svalue);
 }
 
 int getopt_get_int(getopt_t *getopt, const char *lname)
 {
-    const char *v = getopt_get_string(getopt, lname);
+    char *v = getopt_get_string(getopt, lname);
     assert(v != NULL);
-    return atoi(v);
+    int val = atoi(v);
+    free(v);
+    return val;
 }
 
 int getopt_get_bool(getopt_t *getopt, const char *lname)
 {
-    const char *v = getopt_get_string(getopt, lname);
+    char *v = getopt_get_string(getopt, lname);
     assert (v!=NULL);
-    return !strcmp(v, "true");
+    int val = !strcmp(v, "true");
+    free(v);
+    return val;
 }
 
 static int max(int a, int b)
