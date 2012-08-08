4ecab991e00be5cbf5305276a3c90af7b935810b
Johannes H. Strom <jstrom@gmail.com>
2012-08-08 12:11:40 -0400

    Added ability of BilinearRasterizer to handle grayscale images

--- java/src/april/camera/BilinearRasterizer.java
+++ java/src/april/camera/BilinearRasterizer.java
@@ -73,9 +73,64 @@ public class BilinearRasterizer implements Rasterizer
         }
     }
 
+    public BufferedImage rectifyImage(BufferedImage in)
+    {
+        switch(in.getType())
+        {
+            case BufferedImage.TYPE_INT_RGB:
+                return rectifyImageIntRGB(in);
+
+            case BufferedImage.TYPE_BYTE_GRAY:
+                return rectifyImageByteGray(in);
+
+            default:
+                throw new RuntimeException("Unsupported image type: "+in.getType());
+       }
+    }
+
     /** Rectify an RGB BufferedImage using the lookup tables.
       */
-    public BufferedImage rectifyImage(BufferedImage in)
+    public BufferedImage rectifyImageByteGray(BufferedImage in)
+    {
+        int width = in.getWidth();
+        int height = in.getHeight();
+
+        assert(width == inputWidth && height == inputHeight);
+
+        BufferedImage out = new BufferedImage(outputWidth, outputHeight,
+                                              BufferedImage.TYPE_BYTE_GRAY);
+
+        byte _in[]  = ((DataBufferByte) (in.getRaster().getDataBuffer())).getData();
+        byte _out[] = ((DataBufferByte) (out.getRaster().getDataBuffer())).getData();
+
+        for (int i=0; i < _out.length; i++) {
+
+            int idx = indices[i];
+
+            if (idx == -1)
+                continue;
+            int v00 = _in[idx];             // x0, y0
+            int v10 = _in[idx + 1];         // x1, y0
+            int v01 = _in[idx + width];     // x0, y1
+            int v11 = _in[idx + width + 1]; // x1, y1
+
+            int b00 = ((v00      ) & 0xFF) * weights[4*i + 0];
+            int b10 = ((v10      ) & 0xFF) * weights[4*i + 1];
+            int b01 = ((v01      ) & 0xFF) * weights[4*i + 2];
+            int b11 = ((v11      ) & 0xFF) * weights[4*i + 3];
+
+            int b = (b00 + b10 + b01 + b11) >> 16;
+
+            _out[i] =  (byte)(b & 0xFF);
+        }
+
+        return out;
+
+    }
+
+    /** Rectify an RGB BufferedImage using the lookup tables.
+      */
+    public BufferedImage rectifyImageIntRGB(BufferedImage in)
     {
         int width = in.getWidth();
         int height = in.getHeight();
