5ff3ee4e10ead2f17d5376463f12aa936d88b1da
Andrew Richardson <chardson@umich.edu>
2012-12-05 12:53:27 -0500

    CameraCalibrationSystem much closer, but merging subsystems still in progress

--- java/src/april/camera/calibrator/CameraCalibrationSystem.java
+++ java/src/april/camera/calibrator/CameraCalibrationSystem.java
@@ -1,5 +1,10 @@
 package april.camera.calibrator;
 
+// TODO
+//  - use of cameraNumber and cameraIndex not fully fleshed out, especially
+//    for MosaicWrapper data (images and detections that are indexed by cameras.size())
+//  - replace xyzrpy with xyzquat?
+
 /** This class attempts to handle camera and mosaic initialization,
     as well as sub-graph extraction when not enough information is known
     to perform nonlinear optimization on the entire system. Values improved
@@ -16,29 +21,30 @@ package april.camera.calibrator;
 
     initialize camera intrinsics
     {
-        not previously initialized
         initializer returns non-null ParameterizableCalibration
     }
 
-    initialize mosaic extrinsics w.r.t. cameraX
+    initialize mosaic extrinsics w.r.t. cameraX's subsystem
     {
-        not previously initialized
         cameraX's intrinsics have been initialized
+        cameraX's extrinsics have been initialized w.r.t. a known root
         number of detections in cameraX image exceeds minimum
     }
 
     initialize camera extrinsics for cameraX w.r.t. cameraY
     {
-        not previously initialized
-        cameraX's intrinsics have been initialized
-        cameraY's intrinsics have been initialized
         a mosiac exists with extrinsics w.r.t. cameraX
+            \-- depends on: cameraX's intrinsics have been initialized
+
         a mosiac exists with extrinsics w.r.t. cameraY
+            \-- depends on: cameraY's intrinsics have been initialized
     }
 */
-
 public class CameraCalibrationGraph
 {
+    public int REQUIRED_TAGS_PER_IMAGE = 8;   // number of constraints needed per image
+    public static boolean verbose = true;
+
     ////////////////////////////////////////////////////////////////////////////////
     // Data
     TagFamily           tf;
@@ -63,8 +69,8 @@ public class CameraCalibrationGraph
         public ParameterizableCalibration cal; // use get and set methods for parameter lists
 
         // extrinsics
-        public boolean isPriviledgedCamera; // is this the camera at the origin?
-        public double  CameraToGlobalXyzrpy[];
+        public double  CameraToRootXyzrpy[];
+        public int     rootNumber; // the cameraNumber for the root camera
     }
 
     public static class MosaicWrapper
@@ -73,7 +79,12 @@ public class CameraCalibrationGraph
         public List<BufferedImage>      imageSet;
         public List<List<TagDetection>> detectionSet;
 
-        public HashMap<Integer,double[]> MosaicToCameraXyzrpys;
+        // One xyzrpy per subsystem that this mosaic has been connected to.
+        // If a camera's intrinsics were just initialized, it is its own root,
+        // and there will not be an entry in this map for it. If a camera was
+        // already initialized, there may be an entry already from another
+        // camera with the same root
+        public HashMap<Integer,double[]> MosaicToRootXyzrpys;
     }
 
     ////////////////////////////////////////////////////////////////////////////////
@@ -102,9 +113,10 @@ public class CameraCalibrationGraph
             cam.cal = null;
 
             // all cameras are initialized in separate frames at the origin
-            // (they won't have extrinsics at first)
-            cam.isPriviledgedCamera = true;
-            cam.CameraToGlobalXyzrpy = new double[6];
+            // (they won't have "interesting" extrinsics at first). cameras
+            // that are in their own coordinate system have rootNumber == cameraNumber
+            cam.CameraToRootXyzrpy = new double[6];
+            cam.rootNumber = cam.cameraNumber;
         }
 
         // no frames yet
@@ -150,9 +162,9 @@ public class CameraCalibrationGraph
 
             // create a wrapper for the image data
             MosaicWrapper mosaic = new MosaicWrapper();
-            mosaic.imageSet = imageSet;
-            mosaic.detectionSet = detectionSet;
-            mosaic.MosaicToCameraXyzrpys = new HashMap<Integer,double[]>();
+            mosaic.imageSet             = imageSet;
+            mosaic.detectionSet         = detectionSet;
+            mosaic.MosaicToRootXyzrpys  = new HashMap<Integer,double[]>();
 
             mosaics.add(mosaic);
         }
@@ -171,18 +183,81 @@ public class CameraCalibrationGraph
       *
       * Depends on the number of images in the system
       */
-    void updateIntrinsics()
+    private void updateIntrinsics()
     {
+        for (int cameraIndex = 0; cameraIndex < cameras.size(); cameraIndex++)
+        {
+            CameraWrapper cam = cameras.get(cameraIndex);
 
+            // skip if previously initialized
+            if (cam.cal != null)
+                continue;
+
+            List<List<TagDetection>> usableDetections = getCamerasUsableDetections(cameraIndex);
+
+            ParameterizableCalibration cal =
+                cam.initializer.initializeWithObservations(cam.width, cam.height,
+                                                           usableDetections,
+                                                           this.tm);
+
+            // skip if invalid result returned
+            if (cal == null)
+                continue;
+
+            if (verbose)
+                System.out.printf("Initialized intrinsics for camera %d "+
+                                  "(used %d of %d images)\n",
+                                  cameraIndex, usableDetections.size(), mosaics.size());
+
+            cam.cal = cal;
+        }
     }
 
     /** Estimate mosaic-to-camera extrinsics.
       *
-      * Depends on camera intrinsics
+      * Depends on camera intrinsics.
       */
-    void updateMosaicExtrinsics()
+    private void updateMosaicExtrinsics()
     {
+        for (int mosaicIndex = 0; mosaicIndex < mosaics.size(); mosaicIndex++)
+        {
+            MosaicWrapper mosaic = mosaics.get(mosaicIndex);
 
+            for (int cameraIndex = 0; cameraIndex < cameras.size(); cameraIndex++)
+            {
+                CameraWrapper cam = cameras.get(cameraIndex);
+
+                // skip if this camera's intrinsics haven't been initialized
+                if (cam.cal == null)
+                    continue;
+
+                // skip if we've already initialized this mosaic w.r.t. the
+                // root camera in this subsystem
+                if (mosaic.MosaicToRootXyzrpys.get(cam.rootNumber) != null)
+                    continue;
+
+                // skip if this mosaic wasn't observed in the current camera
+                List<TagDetection> detections = mosaic.detectionSet.get(cameraIndex);
+                if (detectionsUsable(detections) == false)
+                    continue;
+
+                double MosaicToCam[][] = estimateMosaicToCameraTransformation(cam.cal, detections);
+
+                double CamToRoot[][] = LinAlg.xyzrpyToMatrix(cam.CameraToRootXyzrpy);
+
+                double MosaicToRoot[][] = LinAlg.matrixAB(CamToRoot,
+                                                          MosaicToCam);
+
+                double xyzrpy[] = LinAlg.matrixToXyzrpy(MosaicToRoot);
+
+                if (verbose)
+                    System.out.printf("Initialized extrinsics for mosaic %d"+
+                                      " using camera %d to root %d\n",
+                                      mosaicIndex, cam.cameraNumber, cam.rootNumber);
+
+                mosaic.MosaicToRootXyzrpys.put(cam.rootNumber, xyzrpy);
+            }
+        }
     }
 
     /** Try to perform a merge if any mosaics define rigid body transformations
@@ -190,7 +265,153 @@ public class CameraCalibrationGraph
       *
       * Depends on camera intrinsics and mosaic extrinsics
       */
-    void updateSubsystemExtrinsics()
+    private void updateSubsystemExtrinsics()
+    {
+        for (int mosaicIndex = 0; mosaicIndex < mosaics.size(); mosaicIndex++)
+        {
+            MosaicWrapper mosaic = mosaics.get(mosaicIndex);
+
+            Set<Integer> rootNumbers = mosaic.MosaicToRootXyzrpys.keySet();
+
+            // if this mosaic isn't currently defined in multiple subsystems,
+            // we can't use it to merge subsystems
+            if (rootNumbers.size() < 2)
+                continue;
+
+            // get camera with lowest unique identifier. All other connected
+            // subsystems will transform into this camera's coordinate frame
+            int rootNumber = rootNumbers.get(0); // start here
+            for (int cameraNumber : rootNumbers)
+                rootNumber = Math.min(rootNumber, cameraNumber);
+
+            CameraWrapper root = getCameraByNumber(rootNumber);
+            assert(root != null);
+
+            double MosaicToRootXyzrpy[] = mosaic.MosaicToRootXyzrpys.get(rootNumber);
+            assert(MosaicToRootXyzrpy != null);
+
+            double MosaicToRoot[][] = LinAlg.xyzrpyToMatrix(MosaicToRootXyzrpy);
+
+            // loop over every other camera connected to this mosaic and transform its subsystem
+            for (int cameraNumber : rootNumbers) {
+                // skip the new root
+                if (cameraNumber == rootNumber)
+                    continue;
+
+                CameraWrapper cam = getCameraByNumber(cameraNumber);
+                assert(cam != null);
+                assert(cam.cameraNumber == cam.rootNumber); // we expected to get a root camera
+
+                double MosaicToCameraXyzrpy[] = mosaic.MosaicToRootXyzrpys.get(cameraNumber);
+                assert(MosaicToCameraXyzrpy != null);
+
+                double MosaicToCamera[][] = LinAlg.xyzrpyToMatrix(MosaicToCameraXyzrpy);
+
+                double CameraToRoot[][] = LinAlg.matrixAB(MosaicToRoot,
+                                                          LinAlg.inverse(MosaicToCamera));
+
+                // XXX should we remove this key/value pair from the hashmap?
+                // be careful to remove correctly, changes in the map will be
+                // reflected in the set
+                //
+                // XXX think more about this
+                transformMosaics(rootNumber, cameraNumber, CameraToRoot);
+                transformCameras(rootNumber, cameraNumber, CameraToRoot);
+
+                if (verbose)
+                    System.out.printf("Connected subsystems for roots %d and %d"+
+                                      "(transformed %d --> %d) using mosaic %d\n",
+                                      rootNumber, cameraNumber,
+                                      cameraNumber, rootNumber,
+                                      mosaicIndex);
+            }
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Helper methods
+
+    private List<List<TagDetection>> getCamerasUsableDetections(int cameraIndex)
+    {
+        List<List<TagDetection>> currentCameraDetections = new ArrayList<List<TagDetection>>();
+
+        for (MosaicWrapper mosaic : mosaics) {
+            List<TagDetection> detections = mosaic.detectionSet.get(cameraIndex);
+
+            if (detectionsUsable(detections)
+                currentCameraDetections.add(detections);
+        }
+
+        return currentCameraDetections;
+    }
+
+    private boolean detectionsUsable(List<TagDetection> detections)
+    {
+        if (detections.size() < REQUIRED_TAGS_PER_IMAGE)
+            return false;
+
+
+        // skip if the tags don't span multiple rows and columns
+        int minRow = this.tm.getRow(detections.get(0).id);
+        int maxRow = minRow;
+        int minCol = this.tm.getColumn(detections.get(0).id);
+        int maxCol = minCol;
+        for (TagDetection d : detections) {
+            minRow = Math.min(minRow, this.tm.getRow(d.id));
+            maxRow = Math.max(maxRow, this.tm.getRow(d.id));
+            minCol = Math.min(minCol, this.tm.getColumn(d.id));
+            maxCol = Math.max(maxCol, this.tm.getColumn(d.id));
+        }
+
+        int rowSpan = maxRow - minRow + 1;
+        int colSpan = maxCol - minCol + 1;
+        if (rowSpan < 2 || colSpan < 2)
+            return false;
+
+        return true;
+    }
+
+    private double[] estimateMosaicToCameraTransformation(ParameterizableCalibration cal,
+                                                          List<TagDetection> detections)
+    {
+        double K[][] = cal.copyIntrinsics();
+
+        ArrayList<double[]> points_mosaic_meters = new ArrayList<double[]>();
+        ArrayList<double[]> points_image_pixels = new ArrayList<double[]>();
+
+        for (TagDetection d : detections) {
+            points_mosaic_meters.add(LinAlg.select(this.tm.getPositionMeters(d.id), 0, 1));
+            points_image_pixels.add(LinAlg.select(d.cxy, 0, 1));
+        }
+
+        double H[][] = CameraMath.estimateHomography(points_mosaic_meters,
+                                                     points_image_pixels);
+
+        double Rt[][] = CameraMath.decomposeHomography(H, K, points_mosaic_meters.get(0));
+
+        return Rt;
+    }
+
+    private CameraWrapper getCameraByNumber(int cameraNumber)
+    {
+        for (CameraWrapper cam : cameras)
+            if (cam.cameraNumber == cameraNumber)
+                return cam;
+
+        return null;
+    }
+
+    private void transformMosaics(int newRootNumber, int oldRootNumber, double OldToNew[][])
+    {
+        for (int mosaicIndex = 0; mosaicIndex < mosaics.size(); mosaicIndex++)
+        {
+            MosaicWrapper mosaic = mosaics.get(mosaicIndex);
+
+
+        }
+    }
+
+    private void transformCameras(int newRootNumber, int oldRootNumber, double OldToNew[][])
     {
 
     }
